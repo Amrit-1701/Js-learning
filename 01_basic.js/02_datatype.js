a29a1e56893c7f17b6accd5a751f80912691276a
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-23 07:01:22 +0000

    n

---
+++ 01_basic.js/02_datatype.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as new version
+
+//alert("hey") we use nodejs intead of browser
+
+console.log("h")
+// mdn, tc39.es
+
+let name="amrit"
+let age=19
+let isLoggedIn=false;
+
+/*diifferent datatypes:
+    number=>2^53
+    bigint 
+    String
+    Boolean
+    null (standalone val , not undefine)
+    undefined
+    symbol->unique
+    **object
+*/
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ 01_basic.js/03_conversionOperation.js
@@ -0,0 +1,8 @@
+let score="6ab5"
+//cann't change in num so chek
+
+console.log(typeof score)
+
+let vaalInNum=Number(score)
+console.log(typeof vaalInNum)
+console.log(vaalInNum)
