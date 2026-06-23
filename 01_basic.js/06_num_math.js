b16cfd9ff2cf5272233281f7cc48d48a8b175f19
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-23 12:14:55 +0000

    n

--- 01_basic.js/06_num_math.js
+++ 01_basic.js/06_num_math.js
@@ -1,1 +1,24 @@
-c
+const num=400.456
+
+const balance=new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(3))
+
+//console.log(num.toPrecision(4));
+
+const num2=45621380
+//console.log(num2.toLocaleString('en-IN'));
+//en-IN is use to use comma as per the indian std 
+
+// +++++++++++++++++++ Maths ++++++++++++++++++++++++++
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.56));
+// console.log(Math.ceil(4.01))
+// console.log(Math.floor(4.5));
+console.log(Math.min(4,8,2,1,6))
+console.log(Math.random());//always btw 0&1
+console.log(Math.random()*10);
+
