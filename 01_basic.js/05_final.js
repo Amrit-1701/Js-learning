efbd790213d966c2eb6503e5a6d21f5a6f963a99
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-23 09:19:26 +0000

    n

--- 01_basic.js/05_final.js
+++ 01_basic.js/05_final.js
@@ -5,3 +5,22 @@
 // Reference (Non Primitive)
 
 //array object Function
+// Stack (primitive, copy provide) , Heap (non-primitive , reference provide)
+
+let myName="aj"
+
+let otherName = myName
+otherName="jiya"
+
+console.log(myName)
+console.log(otherName) //no change in myName as it send only the copy to otherName
+
+let user={
+    email:"kjdkn@gmail.com"
+}
+
+let userOne= user
+userOne.email="jfbejg2gmail.com"
+
+console.log(user.email)
+console.log(userOne.email) //it change the val
