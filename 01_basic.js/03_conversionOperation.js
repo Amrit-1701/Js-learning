64729e9ac7c9aef3c24ae5f46496baf73ddf2857
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-23 07:13:21 +0000

    n

--- 01_basic.js/03_conversionOperation.js
+++ 01_basic.js/03_conversionOperation.js
@@ -1,8 +1,34 @@
-let score="6ab5"
+let score="6ab5" //give Nan
+let score1=null //give 0
+let score2=undefined //give NaN
+let sc=true //give 1
+
 //cann't change in num so chek
 
 console.log(typeof score)
 
 let vaalInNum=Number(score)
-console.log(typeof vaalInNum)
+console.table(typeof vaalInNum)
 console.log(vaalInNum)
+
+let isLoggedIn=1
+
+let boolIsLogg=Boolean(isLoggedIn)
+console.log(boolIsLogg)
+
+/*example-
+   "": false
+   "df": true
+*/
+
+let someNum=556
+
+let conStr=String(someNum)
+console.log(typeof conStr)
+console.log(conStr)
+
+/*
+null-null
+undefine-undefine
+num-str
+*/
