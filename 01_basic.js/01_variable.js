5a8ce0921ddf09aba6a775fcafd194418be1eb70
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-23 06:16:15 +0000

    n

---
+++ 01_basic.js/01_variable.js
@@ -0,0 +1,20 @@
+console.log('hello')
+//constant(no change)
+const acctId = 4569
+let acctEmail="jaindyanamrit@gmail.com"
+var acctPass="jah356iijf"
+acctCity="CHD"
+let acctState
+
+//(comment) acctId=2
+acctEmail="hddn@g.com"
+acctPass="wr3eg"
+acctCity="jaipur"
+
+
+console.log(acctId);
+
+/*prefer not to use "var" bcz of issue in block and func scope.*/
+
+console.table([acctEmail,acctId,acctPass,acctCity,acctState])
+
