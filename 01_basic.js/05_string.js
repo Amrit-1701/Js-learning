48e5f95a60e1832c2d1933c3f293bacbd1b2d98d
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-23 11:44:44 +0000

    new

---
+++ 01_basic.js/05_string.js
@@ -0,0 +1,30 @@
+const name='amrit'
+const repoCount=5
+
+//console.log(name+' '+repoCount+" say hello")
+
+//console.log(`My Name Is ${name} My Repo Cout is ${repoCount} and hello to everyone ${name.length}`)
+
+const gameName=new String('fgfb-j45-fzt')
+//console.log(gameName.toUpperCase())
+//console.log(gameName.charAt('4'))
+//console.log(gameName.indexOf('z'))
+console.log(gameName.split('-'));
+
+
+// let newStr=gameName.substring(0,5)
+// console.log(newStr);
+// let newStr1=gameName.slice(-8,3)
+// console.log(newStr1);
+
+const str="   foo   "
+// console.log(str.trim());
+
+const url = "https.fgh%20google.com"
+console.log(url.replace('%20','-'));
+console.log(url.includes('goog'));
+
+
+
+
+
