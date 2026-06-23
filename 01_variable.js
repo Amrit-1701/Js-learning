6acc8bc6ec8b32d2fe868a487b84a6a02ed49bae
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-23 06:03:00 +0000

    nw

---
+++ 01_variable.js
@@ -0,0 +1,10 @@
+console.log('hello')
+//constant(no change)
+const acctId = 4569
+let acctEmail="jaindyanamrit@gmail.com"
+var acctPass="jah356iijf"
+acctCity="CHD"
+
+acctId=2
+console.log(acctId);
+
