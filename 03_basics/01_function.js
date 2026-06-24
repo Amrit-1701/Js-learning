51d159401dbffe2ccadb1b6b155c0e52db67c6b8
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-24 09:38:35 +0000

    .

---
+++ 03_basics/01_function.js
@@ -0,0 +1,11 @@
+function sayMyName(){
+    console.log("a");
+    console.log("m");
+   
+}
+//sayMyName()
+
+function addnum(num1,num2){
+    console.log(`adittion result is ${num1+num2}`)
+}
+addnum(78,56)
