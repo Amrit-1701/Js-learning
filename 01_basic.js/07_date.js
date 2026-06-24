b6d4e3e014928bc6bcc452a4c7d6478bef88be3c
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-24 05:41:25 +0000

    n

---
+++ 01_basic.js/07_date.js
@@ -0,0 +1,20 @@
+let myDate=new Date()
+console.log(typeof myDate);
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+let myTimeStamp=Date.now()
+//console.log(myTimeStamp);//in milisec
+//console.log(myTimeStamp.toExponential());
+//console.log(myDate.getTime());
+//console.log(Math.floor(Date.now()/1000));//convert in sec
+
+let newDate=new Date()
+// console.log(newDate.toDateString());
+// console.log(newDate.getFullYear());
+// console.log(newDate.getTimezoneOffset());
+// console.log(newDate.getDay());
