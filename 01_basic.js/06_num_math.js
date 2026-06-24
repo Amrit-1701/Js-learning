9cc29317bf5ebcfe189dcf1eae5d8b97b4b638c9
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-24 04:59:48 +0000

    n

--- 01_basic.js/06_num_math.js
+++ 01_basic.js/06_num_math.js
@@ -18,7 +18,11 @@ const num2=45621380
 // console.log(Math.round(4.56));
 // console.log(Math.ceil(4.01))
 // console.log(Math.floor(4.5));
-console.log(Math.min(4,8,2,1,6))
-console.log(Math.random());//always btw 0&1
-console.log(Math.random()*10);
+// console.log(Math.min(4,8,2,1,6))
+// console.log(Math.random());//always btw 0&1
+ console.log((Math.random()*10)+1);
 
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
