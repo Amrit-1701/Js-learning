93440d44bdcc320e4401e057133b74dfac83db95
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-25 10:36:11 +0000

    .

--- 05_iteration/five.js
+++ 05_iteration/five.js
@@ -1,26 +1,46 @@
 //
 
-const coding=['java','cpp','ruby','js','react']
+const coding = ['java', 'cpp', 'ruby', 'js', 'react']
 
 // coding.forEach( function (item){
 //     console.log(item);
-    
+
 // } )
 
 // coding.forEach((item)=>{
 //     console.log(item);
-    
+
 // })
 
 
 // function printMe(item){
 //     console.log(item);
-    
+
 // }
 
 // coding.forEach(printMe) //only reference no execute
 
 
-coding.forEach((item,index,arr)=>{
-    console.log(item,index,arr);
+coding.forEach((item, index, arr) => {
+    // console.log(item,index,arr);
+})
+
+const myCoding = [
+    {
+        langName: 'javascript',
+        landFileName: 'js'
+    },
+    {
+        langName: 'python',
+        landFileName: 'py'
+    },
+    {
+        langName: 'c++',
+        landFileName: 'cpp'
+    }
+]
+
+myCoding.forEach((item) => {
+    console.log(item.langName);
+
 })
