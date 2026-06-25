22a992a8cef416d8e88a969bd6d2d1b73359a5e2
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-25 09:09:20 +0000

    .

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -1,15 +1,32 @@
 //for
 
-for (let index = 0; index <10; index++) {
+for (let index = 0; index < 10; index++) {
     const element = index;
-   
-    if (index==5) {
-       // console.log('best choice');
+
+    if (index == 5) {
+        // console.log('best choice');
     }
-   // console.log(element);
-    
+    // console.log(element);
+
 }
 
-for(let i=0;i<=10;i++){
-    for(let j=)
-}
+//for (let i = 0; i <= 7; i++) {
+//     //console.log(`outer loop val ${i}`);
+//     console.log(`Table  of ${i}`);
+    
+//     for (let j = 0; j <= 10; j++) {
+//         // console.log(`inner looop val ${j} and outer loop ${i}`);
+//         console.log(`${i} * ${j} = ${i * j}`)
+//     }
+// }
+
+
+// let arr=['flash','butcher','batman']
+// for(let i=0;i<arr.length;i++){
+//     let el=arr[i]
+//     console.log(el);
+    
+// }
+
+
+//  
