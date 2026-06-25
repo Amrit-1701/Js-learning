ce2ecd1bef79edf02c70240b543309ff15485fb6
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-25 07:28:42 +0000

    .

---
+++ 04_controlFlow/truthy.js
@@ -0,0 +1,19 @@
+const userEmail={}
+if (userEmail) {
+    console.log("got email");
+    
+}else{
+    console.log("no email");
+    
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
