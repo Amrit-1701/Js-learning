9eda7e939296c19d9c104ef598fb631c8b1f3966
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-25 08:46:56 +0000

    .

---
+++ 05_iteration/one.js
@@ -0,0 +1,15 @@
+//for
+
+for (let index = 0; index <10; index++) {
+    const element = index;
+   
+    if (index==5) {
+       // console.log('best choice');
+    }
+   // console.log(element);
+    
+}
+
+for(let i=0;i<=10;i++){
+    for(let j=)
+}
