65e70d28d40ad06bd34911a4fc46e20c1db322c4
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-25 10:13:15 +0000

    ,

--- 05_iteration/four.js
+++ 05_iteration/four.js
@@ -1,9 +1,35 @@
-//solving obj problem from forof.js file
+//solving obj problem from forof file
 
 const myObj={
     js:'javaScript',
     cpp:'c++',
-    py:'pythhon',
+    py:'python',
     rb:'ruby',
     swift:'swift by apple'
 }
+
+for(const key in myObj){
+    //console.log(`${myObj[key]} shortcut is ${key}`);
+    
+}
+
+const arr=[1,2,3,5]
+for(const i in arr){
+   // console.log(i) // this gives the indexes
+    //console.log(arr[i]);
+    
+}
+
+
+// const map = new Map()
+// map.set('a', 'chd')
+// map.set('b', 'jaipur')
+// map.set('c', 'delhi')
+// map.set('d', 'punjab')
+
+// for(const k in map ){
+//     //console.log(k);//no output
+    
+// }
+
+
