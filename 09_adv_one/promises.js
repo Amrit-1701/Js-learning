f4f807978735ba2b09af3a70031ec12b8cce504e
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-28 11:49:04 +0000

    .

--- 09_adv_one/promises.js
+++ 09_adv_one/promises.js
@@ -2,26 +2,38 @@
 //nhi aya (error hai) so .catch() me aega
 //or .finally() me hamesha aega chahe response aaye ya error aaye
 
-const promiseOne = new Promise(function (resolve, reject) {
-    //do an asynchronous task
-    //DB calls, cryptography, network
+
+// const promiseOne = new Promise(function (resolve, reject) {
+//     //do an asynchronous task
+//     //DB calls, cryptography, network
+//     setTimeout(function () {
+//         //  console.log('async task is complete')
+//         resolve() // this is use to connect the promise with .then() method
+//     }, 1000)
+// })
+// // .then is related to resolve and catch is related to reject
+// promiseOne.then(function () {
+//     //  console.log("promise consumed")
+// })
+
+
+// new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         console.log("async task 2");
+//         resolve()      
+//     },1000)
+// }).then(function(){
+//     console.log('promise consumed 2');   
+// })
+
+
+const promiseThree = new Promise(function (resole, reject) {
     setTimeout(function () {
-      //  console.log('async task is complete')
-        resolve() // this is use to connect the promise with .then() method
+        resolve({username:'amrit', email:'rrf@gmail.com'})
     }, 1000)
 })
 
-// .then is related to resolve and catch is related to reject
-promiseOne.then(function () {
-  //  console.log("promise consumed")
+promiseThree.then(function (user) {
+    console.log(user);
 })
 
-new Promise(function(resolve,reject){
-    setTimeout(function(){
-        console.log("async task 2");
-        resolve()      
-    },1000)
-}).then(function(){
-    console.log('promise consumed 2');
-    
-})
