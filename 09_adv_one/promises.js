6bfc13f714ab88283a077b4fb75d31f086190112
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-28 16:25:28 +0000

    ,

--- 09_adv_one/promises.js
+++ 09_adv_one/promises.js
@@ -71,25 +71,47 @@ promiseFour
 
 
 //+++++++ async & await ++++++++
-const promiseFive = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        let error = true;
-        if (!error) {
-            resolve({ username: 'amrit', email: 'jsfe@gmail.com' })
-        } else {
-            reject('error: Js went wrong')
-        }
-    }, 1000)
+// const promiseFive = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         let error = true;
+//         if (!error) {
+//             resolve({ username: 'amrit', email: 'jsfe@gmail.com' })
+//         } else {
+//             reject('error: Js went wrong')
+//         }
+//     }, 1000)
+// })
+
+// async function consumePromiseFive() {
+//     try {
+//         const response = await promiseFive
+//         console.log(response)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// } //promiseFive is a Object so we do not consume it like a function (promiseFive())
+
+// consumePromiseFive()
+
+
+// async function getAllUser(){
+//     try{
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json() //we use await bcsz this .json convrsion take time and if we do no use it the code show error
+//         console.log(data)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+//getAllUser()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
 })
-
-
-async function consumePromiseFive() {
-    try {
-        const response = await promiseFive
-        console.log(response)
-    } catch (error) {
-        console.log(error)
-    }
-} //promiseFive is a Object so we do not consume it like a function (promiseFive())
-
-consumePromiseFive()
+.then((data)=>{
+    console.log(data)
+})
+.catch((error)=>{
+    console.log(error)
+})
