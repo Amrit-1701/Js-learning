09d0834511b7fe172a30b9569722cea11aa58e7f
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-30 08:51:55 +0000

    .

--- 10_classes_oop/properties_get_set.js
+++ 10_classes_oop/properties_get_set.js
@@ -1,2 +1,26 @@
- 
- 
+function User(email, pass) {
+    this._email = email
+    this._pass = pass
+
+    Object.defineProperty(this, 'email', {
+        get: function () {
+            return this._email.toUpperCase()
+        },
+        set: function (val) {
+            this.email = val
+        }
+    })
+    Object.defineProperty(this, 'pass', {
+        get: function () {
+            return `${this._pass.toUpperCase()}223k`
+        },
+        set: function (val) {
+            this.pass = val
+        }
+    })
+
+}
+
+const news = new User("hfhj@gmail.com", '152;kj')
+console.log(news.email);
+console.log(news.pass);
