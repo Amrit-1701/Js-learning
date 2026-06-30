50e4f33621238e75edbd7db22c4cfef3465f6f2e
Amrit-1701 <jaindyanamrit@gmail.com>
2026-06-30 15:34:39 +0000

    .

--- 10_classes_oop/object_get_set.js
+++ 10_classes_oop/object_get_set.js
@@ -7,8 +7,15 @@ const user = {
     },
     set email(val) {
         this.email = val
+    },
+    get pass(){
+        return `${this._pass.toUpperCase()}`
+    },
+    set pass(val){
+        this._pass = Val
     }
 }
 
 const tea = Object.create(user)
 console.log(tea.email);
+console.log(tea.pass);
